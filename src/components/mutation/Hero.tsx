039eb7358a098cb1d0e3115abf6f47bf2f2fb66a
parsamoloody <parsaprogramerjs@gmail.com>
2025-09-18 13:01:56 +0330

    asfasdf

--- src/components/mutation/Hero.tsx
+++ src/components/mutation/Hero.tsx
@@ -19,10 +19,10 @@ const Hero = ({ content }: { content: any }): JSX.Element => {
                             {content.title}
                         </h1>
                         <p className="">
-                           {content.description[0]}
+                            {content.description[0]}
                         </p>
                         <p className="">
-                           {content.description[1]}
+                            {content.description[1]}
                         </p>
 
                     </div>
@@ -38,18 +38,17 @@ const Hero = ({ content }: { content: any }): JSX.Element => {
                         />
                     </div>
                 </div>
-                 {/* Line */}
+                {/* Line */}
                 <div className='w-full text-center my-14 lg:px-40 lg:my-40'>
                     <div className='text-2xl text-center h-full lg:mx-auto text-gray-900 p-1 inline'>
-                       {content.linear}
+                        {content.linear}
                     </div>
                 </div>
                 {content.intro.map((section: any, index: number) => (
-                    <div 
-                        key={index} 
-                        className={`flex flex-col lg:flex-row gap-8 items-center ${
-                            index % 2 === 1 ? 'lg:flex-row-reverse' : ''
-                        }`}
+                    <div
+                        key={index}
+                        className={`flex flex-col lg:flex-row gap-8 items-center ${index % 2 === 1 ? 'lg:flex-row-reverse' : ''
+                            }`}
                     >
                         <div className="flex-1 space-y-6 text-lg">
                             <p className='text-lg text-gray-600 border-b border-red-700 p-1 inline'>
@@ -72,6 +71,7 @@ const Hero = ({ content }: { content: any }): JSX.Element => {
                                     width={1200}
                                     height={800}
                                     className="w-full h-auto object-cover"
+                                    priority
                                     placeholder='blur'
                                 />
                             ))}
